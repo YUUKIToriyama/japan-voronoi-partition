ca3e346c39a6d3a83ba0973fecf60de201793f8c
YUUKIToriyama <github@toriyama.dev>
2021-10-09 20:54:38 +0900

    draw voronoi

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "../public/style.scss";
 import maplibregl from 'maplibre-gl';
+import * as Turf from '@turf/helpers';
+import voronoi from '@turf/voronoi';
 
 const displayMap = (args: { id: string, styleFile: string, center: { lng: number, lat: number } }) => {
 	const map = new maplibregl.Map({
@@ -20,6 +22,7 @@ const plotPrefCapitals = async () => {
 	const endpoint = "https://geolonia.github.io/japanese-prefectural-capitals/";
 	const result: Record<string, Record<"lat" | "lng", number>> = await fetch(endpoint).then(response => response.json());
 	for (let prefecture of Object.entries(result)) {
+		// 県庁所在地をプロット
 		const popup = new maplibregl.Popup({});
 		popup.setHTML(`<h3>${prefecture[0]}</h3>`);
 		const marker = new maplibregl.Marker();
@@ -27,8 +30,7 @@ const plotPrefCapitals = async () => {
 		marker.setPopup(popup);
 		marker.addTo(map);
 	}
-	console.log(result);
-	console.log(result["北海道"].lat)
+	return result;
 }
 
 const map = displayMap({
@@ -38,5 +40,29 @@ const map = displayMap({
 		lat: 35.7980668,
 		lng: 136.4041623
 	}
-})
-plotPrefCapitals()
+});
+
+plotPrefCapitals().then(points => {
+	const pointList = Object.values(points).map(point => {
+		return Turf.point([point.lng, point.lat]);
+	});
+	const voronoiPolygons = voronoi(Turf.featureCollection(pointList), {
+		bbox: [122.5557, 20.2531, 153.5912, 45.3326]
+	});
+	console.log(voronoiPolygons);
+	map.on("load", () => {
+		map.addSource("voronoi", {
+			type: "geojson",
+			data: voronoiPolygons
+		});
+		map.addLayer({
+			id: "voronoi",
+			type: "fill",
+			source: "voronoi",
+			paint: {
+				"fill-color": "#009911",
+				"fill-opacity": 0.5
+			}
+		});
+	})
+});
